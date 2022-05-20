d04a2b620ba8c569cbb94ebcf427cc1043adc809
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2022-05-20 18:19:29 -0300

    Version

--- src/Elements.js
+++ src/Elements.js
@@ -2,17 +2,24 @@ import React ,{ useState }from "react";
 import "./Todo.css";
 import List from "./List";
 import TodoForm from "./todoForm";
+import Item from "./Item";
 function Elements(){
 
     const [items,setItems] = useState([]);
-    function onAddItem(item){
-        setItems([...items, item])
+    function onAddItem(text){
+        let it = new Item(text)
+        setItems([...items, it])
+    }
+    function onItemDeleted(item){
+        let FiltredItems = items.filter(it =>it.id != item.id);
+        setItems(FiltredItems)
+
     }
     return(
         <div className="Containers">
                 <h1 id="Title">Todo List</h1>
                 <TodoForm onAddItem={onAddItem}></TodoForm>
-                <List items={items}></List>
+                <List onItemDeleted={onItemDeleted} items={items}></List>
         </div>
 
     )
--- src/List.js
+++ src/List.js
@@ -3,7 +3,12 @@ import React from "react";
 function List(props){
     return(
         <ul className="ContainerList">
-            {props.items.map(item =><li className="Base" key={item} >{item}</li>)}
+            {props.items.map(item =>
+            <li className="Base" key={item}>{item.text}
+            <button onClick={()=>{props.onItemDeleted(item)}}>
+            <img src="./lixo.jpg" alt=""></img></button>
+            </li>)}
+            
         </ul>
     )
 }
